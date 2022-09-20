f22e734f7a028150968450495805ff9b28c08a81
Julio Mayen <mayenrosil@gmail.com>
2022-09-19 21:13:39 -0600

    constructor

--- proyectofinal/src/main/java/com/programacion2/proyectofinal/Vistas/GestionUsuarios.java
+++ proyectofinal/src/main/java/com/programacion2/proyectofinal/Vistas/GestionUsuarios.java
@@ -31,6 +31,10 @@ public class GestionUsuarios extends javax.swing.JFrame {
         sqlGestionUsuario.listarUsuarios(tblUsuarios);
     }
 
+    private GestionUsuarios() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
